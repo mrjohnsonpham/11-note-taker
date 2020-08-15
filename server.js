256f27803ebe4ce563a62ddb2fb84225651de4b4
Johnson Pham <mrjohnsonpham@gmail.com>
2020-08-15 17:40:14 -0400

    App succesfully listening on assigned PORT

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+
+const app = express();
+
+var PORT = process.env.PORT || 3000; 
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static(__dirname + ' public'));
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
